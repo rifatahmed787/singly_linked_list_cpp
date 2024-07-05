803648fcbc1b6d0a04862c6d911f2c54c5025b6c
Md Rifat <mdrifatahmed787@gmail.com>
2024-07-05 16:29:56 +0600

    cycle detected

---
+++ cycle_detect.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+   int val;
+   Node* next;
+
+   Node(int val){
+    this->val=val;
+    this->next=NULL;
+   }
+};
+ 
+int main(){
+    Node *head=new Node(10);
+    Node *a =new Node(20);
+    Node *b =new Node(30);
+    Node *c=new Node(40);
+    
+    head->next=a;
+    a->next=b;
+    b->next=c;
+    c->next=a;
+
+    Node *slow=head;
+    Node *fast=head;
+     
+    while(slow!=NULL && fast->next != NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+
+        if(slow==fast){
+            cout<<"cycle detected"<<endl;
+            break;
+        }
+    }
+    return 0;
+}
